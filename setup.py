a1d1974f26a706b59c66af8bc3c96545d6e41f45
kujosHeist <shane.carty@hotmail.com>
2018-02-13 22:18:11 +0000

    changed structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tle2czml',
+      version='0.1',
+      description='Convert TLE\'s to CZML file',
+      url='https://github.com/kujosHeist/tle2czml',
+      author='Shane Carty',
+      author_email='shane.carty@hotmail.com',
+      license='MIT',
+      packages=['tle2czml'],
+      include_package_data=True,
+      zip_safe=False)
