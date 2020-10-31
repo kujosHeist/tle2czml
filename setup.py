7db218e8a58eaa6177a611293e761869071a0135
Shane Carty <shane.carty@du.co>
2020-10-31 18:08:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 ''' script to be used by setuptools to build package '''
+import setuptools
 
-from setuptools import setup
+with open("README.md", "r") as file:
+    LONG_DESCRIPTION = file.read()
 
-setup(name='tle2czml',
-      version='0.3',
-      description='Convert TLE\'s to CZML file',
-      url='https://github.com/kujosHeist/tle2czml',
-      author='Shane Carty',
-      author_email='shane.carty87@gmail.com',
-      license='MIT',
-      packages=['tle2czml'],
-      install_requires=[
-          'pygeoif>=0.7',
-          'python-dateutil>=2.6.1',
-          'pytz>=2018.3',
-          'sgp4>=1.4',
-          'six>=1.11.0',
-          'wheel>=0.24.0',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setuptools.setup(
+    name='tle2czml',
+    version='0.3',
+    author='Shane Carty',
+    author_email='shane.carty87@gmail.com',
+    description='Convert TLE\'s to CZML file',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kujosHeist/tle2czml',
+    packages=['tle2czml'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pygeoif>=0.7',
+        'python-dateutil>=2.6.1',
+        'pytz>=2018.3',
+        'sgp4>=1.4',
+        'six>=1.11.0',
+        'wheel>=0.24.0',
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
