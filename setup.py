653482d269e746a97d2a74542879fbd3f8cd99bc
Shane Carty <shane.carty@du.co>
2020-10-31 17:55:04 +0000

    Add licence, remove exact version restrictions for dependencies, bump version number

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup
 
 setup(name='tle2czml',
-      version='0.2',
+      version='0.3',
       description='Convert TLE\'s to CZML file',
       url='https://github.com/kujosHeist/tle2czml',
       author='Shane Carty',
-      author_email='shane.carty@hotmail.com',
+      author_email='shane.carty87@gmail.com',
       license='MIT',
       packages=['tle2czml'],
       install_requires=[
-          'pygeoif==0.7',
-          'python-dateutil==2.6.1',
-          'pytz==2018.3',
-          'sgp4==1.4',
-          'six==1.11.0',
-          'wheel==0.24.0',
+          'pygeoif>=0.7',
+          'python-dateutil>=2.6.1',
+          'pytz>=2018.3',
+          'sgp4>=1.4',
+          'six>=1.11.0',
+          'wheel>=0.24.0',
       ],
       include_package_data=True,
       zip_safe=False)
